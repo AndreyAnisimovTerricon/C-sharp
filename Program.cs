bd3a6415a500a8c6fd6e6c2e971909ba3a1ba300
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-08-20 13:36:31 +0300

    программа сравнения двух чисел

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input number a");
+int a= int.Parse(Console.ReadLine());
+Console.WriteLine("Input number b");
+int b= int.Parse(Console.ReadLine());
+int max = a;
+if (a > b)
+
+    Console.WriteLine("a=max,b=min");
+else
+
+
+    Console.WriteLine("a=min,b=max");
